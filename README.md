93b618bd70c6ce187f5ce13e9a2d4a3f41f2370e
zlp <www.135021918@qq.com>
2018-01-03 17:55:15 +0800

    #add angular material2

--- README.md
+++ README.md
@@ -17,6 +17,21 @@ cnpm install @angular/compiler-cli@latest --save-dev
 https://github.com/dimpu/ngx-md
 
 // update node_modules
+```
 cnpm i -g npm-check-updates
 npm-check-updates -u
 cnpm install
+```
+
+2. 
+```angular2html
+cnpm install --save @angular/material @angular/cdk
+
+cnpm install --save @angular/animations
+
+cnpm install --save hammerjs
+```
+
+
+3. ADD 
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { MarkdownModule } from 'ngx-md';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -11,6 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule,
     MarkdownModule.forRoot()
   ],
   providers: [],
